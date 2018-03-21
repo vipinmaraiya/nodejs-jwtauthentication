3489f241c53a70e8ee370018f8783f41b5aee8c8
Vipin Sharma <vipinsharmaec@hotmail.com>
2018-03-22 00:29:58 +0530

    Add post users routes and token method

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -1,80 +1,30 @@
 // const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db)=>{
-    if(err){
-        return console.log('Unable to connect to MongoDB server');
-    }
-    console.log('Connected to MongoDB server');
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-    // db.collection("Users").find({
-    //     _id: new ObjectID('5aa81fb43484c61164e642ad')
-    // }).toArray().then((docs)=>{
-    //     console.log(docs);
-    // });
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('57bb36afb3b6a3801d8c479d')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
 
-    // db.collection("Users").find({
-    //     location:"Agra"
-    // }).toArray().then((docs)=>{
-    //     console.log(JSON.stringify(docs, undefined,2));
-    // }, (err)=>{
-    //     console.log('Unable to fetch Users', err);
-    // });
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
 
-    // db.collection("Users").find({
-    //     name:"Vipin"
-    // }).toArray().then((result)=>{
-    //     console.log(result);
-    // })
+  db.collection('Users').find({name: 'Andrew'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  });
 
-
-    //deleteMany
-    // db.collection("Users").deleteMany({
-    //     name:"Vipin"
-    // }).then((result)=>{
-    //     console.log(result);
-    // })
-
-
-    //deleteone
-    // db.collection("Users").deleteOne({
-    //     name:"Vipin"
-    // }).then((result)=>{
-    //     console.log(result);
-    // })
-
-    //findoneanddelete
-    // db.collection("Users").findOneAndDelete({
-    //     name:"ZVipin2"
-    // }).then((result)=>{
-    //     console.log(result);
-    // });
-
-    //findOneAndUpdate
-    // db.collection("Users").findOneAndUpdate({
-    //     _id:new ObjectID("5aa82199a64e1b06ec71146e")
-    // },{
-    //     name:"Vipin"
-    // },{
-    //     returnOriginal:false
-    // }).then((result)=>{
-    //     console.log(result);
-    // })
-
-    db.collection("Users").findOneAndUpdate({
-        _id:new ObjectID("5aa82199a64e1b06ec71146e")
-    },{
-        $set:{
-            name:"Vipin2"
-        },
-        $inc:{
-            age:2
-        }
-    },{
-        returnOriginal:false
-    }).then((result)=>{
-        console.log(result);
-    })
-
-    db.close();
+  // db.close();
 });
--- server/models/todo.js
+++ server/models/todo.js
@@ -1,20 +1,20 @@
-const { mongoose } = require("../db/mongoose");
+var mongoose = require('mongoose');
 
-let Todo = mongoose.model("Todo", {
-    text:{
-        type: String,
-        required:true,
-        trim: true,
-        minlength:1
-    },
-    completedAt:{
-        type: Number,
-        default: null
-    },
-    completed:{
-        type:Boolean,
-        default:false
-    }
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
 });
 
 module.exports = {Todo};
